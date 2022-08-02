973010e1fb90d87c3b2f9b53bd69d65036099d3f
Jonathas Leontino Medina <emaildojonathas@gmail.com>
2022-08-02 11:45:42 -0400

    Initial commit

---
+++ app/src/main/java/com/example/aulas127/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.aulas127;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edUsuario, edSenha;
+    Button btLogar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edUsuario = findViewById(R.id.editTextUsuario);
+        edSenha = findViewById(R.id.editTextSenha);
+        btLogar = findViewById(R.id.button);
+
+        btLogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.e("oncreate"," Clique no botão ok");
+
+                String usuario = edUsuario.getText().toString();
+                String senha = edSenha.getText().toString();
+
+                Log.e("oncreate"," Usuario: " + usuario + ". " +
+                        "Senha: " + senha);
+
+                Toast.makeText(
+                        getApplicationContext(),
+                        "Usuario informado foi: " + usuario + ". " +
+                                "Senha digitada foi: " + senha,
+                        Toast.LENGTH_SHORT
+                ).show();
+
+            }
+        });
+
+
+
+
+    }
+}
