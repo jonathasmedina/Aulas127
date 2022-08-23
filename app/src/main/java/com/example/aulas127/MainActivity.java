1343b6ef8ab28593e919bb64ceda62a6874b3d09
Jonathas Leontino Medina <emaildojonathas@gmail.com>
2022-08-23 10:53:23 -0400

    -

--- app/src/main/java/com/example/aulas127/MainActivity.java
+++ app/src/main/java/com/example/aulas127/MainActivity.java
@@ -8,12 +8,16 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText edUsuario, edSenha;
     Button btLogar;
+    String usuario;
+    String senha;
+    TextView tvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
         edUsuario = findViewById(R.id.editTextUsuario);
         edSenha = findViewById(R.id.editTextSenha);
         btLogar = findViewById(R.id.button);
+        tvResult  = findViewById(R.id.textView);
+
         //TODO validar campos
 
         btLogar.setOnClickListener(new View.OnClickListener() {
@@ -30,26 +36,31 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Log.e("oncreate"," Clique no botão ok");
 
-                String usuario = edUsuario.getText().toString();
-                String senha = edSenha.getText().toString();
+                 usuario = edUsuario.getText().toString();
+                 senha = edSenha.getText().toString();
 
                 Log.e("oncreate"," Usuario: " + usuario + ". " +
                         "Senha: " + senha);
-
-                //Toast.makeText(getApplicationContext(),"Usuario informado foi: " + usuario + ". " +"Senha digitada foi: " + senha,Toast.LENGTH_SHORT).show();
-
-               Intent i = new Intent(
-                       MainActivity.this,
-                       MainActivity2.class
-               );
-
-               startActivity(i);
-
+                if(validar()){
+                    Intent i = new Intent(
+                            MainActivity.this,
+                            MainActivity2.class
+                    );
+                    startActivity(i);
+                }
             }
         });
+    }
 
-
-
-
+    private boolean validar() {
+        if(usuario.trim().equals("")){
+            edUsuario.setError("Digite!!!");
+            return false;
+        }
+        if(senha.trim().equals("")){
+            edSenha.setError("Digite!!!");
+            return false;
+        }
+        return true;
     }
 }
